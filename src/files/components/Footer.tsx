c4446c8c3179bd4f1d0d4c7a3be34974ba5e0e99
fizzyprojects <211967213+fizzy-projects@users.noreply.github.com>
2026-03-06 17:59:19 +0800

    added id for footer email to enable href of email icon in header

--- src/files/components/Footer.tsx
+++ src/files/components/Footer.tsx
@@ -8,7 +8,7 @@ export const Footer: React.FC = ()=>{
             <div className="footer-contacts">
                 {/* <a href="#" className="footer-link">GitHub</a> */}
                 <a href="https://www.linkedin.com/in/muhammad-hafiz-bin-mohd-aziz-6274991ab/" className="footer-contacts-link">LinkedIn</a>
-                <p className="footer-contacts-text">Contact me at mmhafiz@gmail.com</p>
+                <p id="email" className="footer-contacts-text">Contact me at mmhafiz@gmail.com</p>
             </div>
         </div>
     </footer>
