25986744319fb92b617efa488fe1442b72c8bd2c
fizzyprojects <211967213+fizzy-projects@users.noreply.github.com>
2026-03-06 17:56:57 +0800

    added href for socials

--- src/files/components/Footer.tsx
+++ src/files/components/Footer.tsx
@@ -5,10 +5,10 @@ export const Footer: React.FC = ()=>{
             <p className="footer-text">
             Crafted with precision and curiosity. Always learning, always questioning.
             </p>
-            <div className="footer-links">
-            <a href="#" className="footer-link">GitHub</a>
-            <a href="#" className="footer-link">LinkedIn</a>
-            <a href="#" className="footer-link">Email</a>
+            <div className="footer-contacts">
+                {/* <a href="#" className="footer-link">GitHub</a> */}
+                <a href="https://www.linkedin.com/in/muhammad-hafiz-bin-mohd-aziz-6274991ab/" className="footer-contacts-link">LinkedIn</a>
+                <p className="footer-contacts-text">Contact me at mmhafiz@gmail.com</p>
             </div>
         </div>
     </footer>
